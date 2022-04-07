48a9c8dda9209408b4d352251e22023e1988baa0
Collin2245 <49080329+Collin2245@users.noreply.github.com>
2022-04-06 22:33:04 -0500

    Add quit functionality

---
+++ src/Server/CORE.java
@@ -0,0 +1,30 @@
+package Server;
+
+import java.io.PrintStream;
+import java.util.ArrayList;
+
+public class CORE 
+{
+    private static ArrayList<PrintStream> theClientStreams = new ArrayList<PrintStream>();
+    
+    public static synchronized void addClientThreadPrintStream(PrintStream ps)
+    {
+        System.out.println("adding client thread");
+        CORE.theClientStreams.add(ps);
+    }
+
+    public static synchronized void removeClientThreadPrintStream(PrintStream ps)
+    {
+        System.out.println("removing client thread");
+        CORE.theClientStreams.remove(ps);
+    }
+
+    public static void broadcastMessage(String message)
+    {
+        System.out.println("About to broadcast....");
+        for (PrintStream ps : CORE.theClientStreams)
+        {
+            ps.println(message);
+        }
+    }
+}
--- src/Server/chatServer.java
+++ src/Server/chatServer.java
@@ -1,29 +1,24 @@
-import java.io.*;
-import java.net.*;
+package Server;
+
 import java.io.PrintStream;
 import java.net.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Server {
-
-
-
-
+public class chatServer
+{
     public static void main(String[] args) throws Exception
     {
         ServerSocket s = new ServerSocket(2222);
-        ArrayList<ClientThread> theThreads = new ArrayList<ClientThread>();
+        ArrayList<ChatWorkerThread> theThreads = new ArrayList<ChatWorkerThread>();
         while(true)
         {
             System.out.println("Listenning for Connection...");
             Socket client = s.accept(); //blocks
-            ClientThread t = new ClientThread(client);
+            ChatWorkerThread t = new ChatWorkerThread(client);
             theThreads.add(t);
             t.start();
         }
-
+        
     }
-
-
 }
